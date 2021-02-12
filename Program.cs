53361012011abd58fdbd0cf1eb92c2cf4f64d451
ellerish <ellerish11@gmail.com>
2021-02-12 15:40:34 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileSystemManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
